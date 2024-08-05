ff0a1603ac4e2691d4708c227fe1bd601b7cf413
Hasnain49 <hasnain72.qureshi@gmail.com>
2024-08-05 16:32:28 +0500

    Delete-todo-done

--- frontend/src/components/Todos.tsx
+++ frontend/src/components/Todos.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useTodos, useTodosIds } from '../services/queries'
-import { useCreateTodo } from '../services/mutations'
+import { useCreateTodo, useDeleteTodo, useUpdateTodo } from '../services/mutations'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { Todo } from '../types/todo'
 
@@ -8,9 +8,15 @@ import { Todo } from '../types/todo'
 const Todos = () => {
     const todosIdsQuery = useTodosIds()
 
-    const createTodoMutation = useCreateTodo()
+    const createTodoMutation = useCreateTodo();
+    
+    const updateTodoMutation = useUpdateTodo();
+    const deleteTodoMutation = useDeleteTodo();
 
-    const {register, handleSubmit} = useForm<Todo>()
+    const {register, handleSubmit, reset} = useForm<Todo>()
+    if(createTodoMutation.isSuccess){
+      // reset(undefined)
+    }
     const todosQueries = useTodos(todosIdsQuery.data)
 
     if(todosIdsQuery.isLoading){
@@ -24,10 +30,30 @@ const Todos = () => {
     const handleCreateTodoSubmit: SubmitHandler<Todo> = (data)=>{
         createTodoMutation.mutate(data);
     }
+
+    const handleMarkAsDone = (data: Todo | undefined) =>{
+      if(data){
+        updateTodoMutation.mutate({...data, checked: true})
+      }
+    }
+
+    const handleDeleteTodoSubmit = async(id: number)=>{
+      await deleteTodoMutation.mutateAsync(id)
+    }
   return (
     <>
     {/* {todosIdsQuery.data?.map((id)=> (<p key={id}>ID: {id}</p>))} */}
-    <form onSubmit={handleSubmit(handleCreateTodoSubmit)}></form>
+    <form onSubmit={handleSubmit(handleCreateTodoSubmit)}>
+      <h4>Todo Form</h4>
+      <label htmlFor="title">Title</label>
+      <input type="text" placeholder='Enter title' id='title' {...register('title')}/>
+      <br />
+      <label htmlFor="desc">Description</label>
+      <input type="text" placeholder='Description' id='desc' {...register("description")}/>
+      <br />
+
+      <button type="submit" disabled={createTodoMutation.isPending}>{createTodoMutation.isPending ? "creating..." : "Submit"}</button>
+    </form>
      <ul>
        {todosQueries.map(({data})=>(
         <li key={data?.id}>
@@ -36,6 +62,10 @@ const Todos = () => {
             <strong>Title</strong> {data?.title}
             <strong>Description</strong> {data?.description}
           </span>
+          <div>
+            <button onClick={()=> handleMarkAsDone(data)} disabled={data?.checked}>{data?.checked? "Done" : "Mark as done"}</button>
+            {data && data?.id && <button onClick={()=> handleDeleteTodoSubmit(data.id!)}>Delete</button>}
+          </div>
         </li>
        ))}
      </ul>
--- frontend/src/services/mutations.ts
+++ frontend/src/services/mutations.ts
@@ -1,9 +1,63 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Todo } from "../types/todo";
-import { createTodo } from "./api";
+import { createTodo, deleteTodo, updateTodo } from "./api";
 
 export function useCreateTodo(){
+    const queryClient = useQueryClient();
     return useMutation({
-        mutationFn: (data: Todo)=> createTodo(data)
+        mutationFn: (data: Todo)=> createTodo(data),
+        onMutate: ()=>{
+            console.log("mutate")
+        },
+        onError:()=>{
+            console.log('error')
+        },
+        onSuccess: ()=>{
+            console.log("success")
+        },
+        onSettled: async(_, error)=>{
+            console.log("settled")
+            if(error){
+                console.log(error)
+            }
+            else{
+                await queryClient.invalidateQueries({queryKey: ["todos"]})
+            }
+        }
+    })
+}
+
+export function useUpdateTodo(){
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (data: Todo)=> updateTodo(data),
+        onSettled: async(_, error, variables)=>{
+            if(error){
+                console.log(error)
+            }
+            else{
+                await queryClient.invalidateQueries({queryKey: ["todos"]}),
+                await queryClient.invalidateQueries({queryKey: ['todo', {id: variables.id}]})
+            }
+        }
+    })
+}
+
+export function useDeleteTodo(){
+    const QueryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (id: number)=> deleteTodo(id),
+        onSuccess: ()=>{
+            console.log('Successfully Deleted')
+        },
+
+        onSettled: async(_, error)=>{
+            if(error){
+                console.log(error)
+            }
+            else{
+                await QueryClient.invalidateQueries({queryKey:['todos']})
+            }
+        }
     })
 }
